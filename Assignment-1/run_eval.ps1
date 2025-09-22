547a884c6b74e1626b0b33c421313c9d96f9cded
Deevyanshu <22b1029@iitb.ac.in>
2025-09-22 21:58:36 +0530

    final commit

--- Assignment-1/run_eval.ps1
+++ Assignment-1/run_eval.ps1
@@ -1,8 +1,7 @@
 # Define parameter ranges
 $K_values = @(5, 10, 20)
-$tasks = @(1, 2, 3)
+$tasks = @(1, 2, 3, 5)
 $datasets = @("arguana", "kialo", "opinionqa")
-# $lambd = 0.5
 
 # Loop through all combinations
 foreach ($dataset in $datasets) {
@@ -31,6 +30,19 @@ foreach ($dataset in $datasets) {
         }
     }
 }
+$lambds_chosen = @{
+    "arguana" = 0.4
+    "kialo" = 0.4
+    "opinionqa" = 0.5
+}
+
+foreach ($dataset in $datasets) {
+    foreach ($K in $K_values) {
+        $lambd = $lambds_chosen[$dataset]
+        Write-Host "Running experiment: Dataset=$dataset, K=$K, Lambda=$lambd"
+        python .\eval.py --dataset $dataset --K $K --task 4 --lambd $lambd
+        Start-Sleep -Seconds 1
+    }
+}
 
 Write-Host "All experiments completed."
-# python .\tasks.py --dataset arguana --K 5 --task 3 --lambd 0.5
--- Assignment-1/run_expts.ps1
+++ Assignment-1/run_expts.ps1
@@ -1,9 +1,7 @@
 # Define parameter ranges
 $K_values = @(5, 10, 20)
-# $tasks = @(1, 2, 3, 4)
-$tasks = @(5)
+$tasks = @(1, 2, 3, 5)
 $datasets = @("arguana", "kialo", "opinionqa")
-$lambd = 0.5
 
 # Loop through all combinations
 foreach ($dataset in $datasets) {
@@ -12,22 +10,21 @@ foreach ($dataset in $datasets) {
 
             Write-Host "Running experiment: Dataset=$dataset, K=$K, Task=$task"
             
-            python .\tasks.py --dataset $dataset --K $K --task $task --lambd $lambd
+            python .\tasks.py --dataset $dataset --K $K --task $task --time
             
             Start-Sleep -Seconds 1
         }
     }
 }
 
-# $lambd_values = @(0.0, 0.1, 0.2, 0.3, 0.4, 0.6, 0.7, 0.8, 0.9, 1.0)
+$lambd_values = @(0.0, 0.1, 0.2, 0.3, 0.4, 0.6, 0.7, 0.8, 0.9, 1.0)
 
-# foreach ($K in $K_values) {
-#     foreach ($lambd in $lambd_values) {
-#         Write-Host "Running experiment with lambda=$lambd"
-#         # Run the Python script with the current parameters
-#         python .\tasks.py --dataset kialo --K $K --task 4 --lambd $lambd
-#     }
-# }
+foreach ($K in $K_values) {
+    foreach ($lambd in $lambd_values) {
+        Write-Host "Running experiment with lambda=$lambd"
+        # Run the Python script with the current parameters
+        python .\tasks.py --dataset kialo --K $K --task 4 --lambd $lambd
+    }
+}
 
 Write-Host "All experiments completed."
-# python .\tasks.py --dataset arguana --K 5 --task 3 --lambd 0.5
