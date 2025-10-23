a4be692a4f6e21dacf9c1a70d091a7473d845866
Deevyanshu <22b1029@iitb.ac.in>
2025-10-23 22:00:17 +0530

    pairs for task 3 and 4

--- Assignment-2/create_index.ps1
+++ Assignment-2/create_index.ps1
@@ -4,5 +4,14 @@ python -m pyserini.index.lucene `
   --generator DefaultLuceneDocumentGenerator `
   --collection JsonCollection `
   --language en `
-  --threads 4 `
+  --threads 8 `
+  --storePositions --storeDocvectors --storeRaw
+
+python -m pyserini.index.lucene `
+  --input data/docs_test `
+  --index wikiclir_index `
+  --generator DefaultLuceneDocumentGenerator `
+  --collection JsonCollection `
+  --language en `
+  --threads 8 `
   --storePositions --storeDocvectors --storeRaw
